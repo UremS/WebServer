c95b0000c09fb46ce816426b0819e20b446707a5
Snow Ball <36713744+UremS@users.noreply.github.com>
2018-03-21 13:42:17 +0300

    Update Program.cs

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -10,7 +10,34 @@ namespace WebServer
     {
         static void Main(string[] args)
         {
+          HttpListener httplistener = new HttpListener();
+            httplistener.Prefixes.Add("http://localhost:3030/");
+            httplistener.Start();
 
+            while (true)
+            {
+                Console.WriteLine("İstemci belkeniyor...");
+                HttpListenerContext cx = httplistener.GetContext();
+
+                Console.WriteLine("İstemci bağlandı.");
+
+                HttpListenerRequest req = cx.Request;
+                HttpListenerResponse res = cx.Response;
+
+                Console.WriteLine("İstenen Adres: ");
+                Console.WriteLine(req.Url);
+
+                string veri = "Hosgeldiniz. Saat: " + DateTime.Now.ToLongTimeString();
+                veri += "<br/>";
+                veri += "Istediginiz Adres: " + req.RawUrl;
+
+                byte[] gonderilecekVeri = Encoding.UTF8.GetBytes(veri);
+
+                res.AddHeader("Content-Type", "text/html; charset=uft-8");
+
+                res.OutputStream.Write(gonderilecekVeri, 0, gonderilecekVeri.Length);
+
+                cx.Response.Close();
         }
     }
 }
